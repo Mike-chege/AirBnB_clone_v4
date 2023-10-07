bc74df6d962939abb707d4454af09d8bad5e95b4
Mike-chege <mikechege93@gmail.com>
2023-10-07 11:47:26 +0000

    Added a query string to fix asset caching

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
-Implementing the
-Flask App that integrates
-with AirBnB static HTML Templates
+Implementing the Flask app
 """
 from flask import Flask, render_template, url_for
 from models import storage
@@ -17,8 +15,7 @@ host = '0.0.0.0'
 @app.teardown_appcontext
 def teardown_db(exception):
     """
-    after each request, this method calls .close() (i.e. .remove()) on
-    the current SQLAlchemy Session
+    Handles SQLAlchemy sessions
     """
     storage.close()
 
@@ -26,7 +23,7 @@ def teardown_db(exception):
 @app.route('/0-hbnb/')
 def hbnb_filters(the_id=None):
     """
-    handles request to custom template with states, cities & amentities
+    Handles custom template requests
     """
     state_objs = storage.all('State').values()
     states = dict([state.name, state] for state in state_objs)
@@ -45,7 +42,6 @@ def hbnb_filters(the_id=None):
 
 if __name__ == "__main__":
     """
-    The main Flask application
-    Should not run imported
+    Should not run if imported
     """
     app.run(host=host, port=port)
